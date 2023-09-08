0e7ca76757ffb1a2d420286f9f9d8d0e14ffa3ed
EC2 Default User <ec2-user@ip-172-31-18-139.ec2.internal>
2023-09-08 07:27:30 +0000

    [fix]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,5 +20,3 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:planner_name] )
   end
 end
-
-end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
+
   def dash_board
   end
 
   def withdrawal
   end
+
+  def destroy
+    current_user.destroy
+    redirect_to root_path
+  end
+
 end
