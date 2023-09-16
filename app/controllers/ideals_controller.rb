3e5a17fb73046a3ff52ca60752daa5e99fb5daac
EC2 Default User <ec2-user@ip-172-31-13-194.ap-northeast-1.compute.internal>
2023-09-16 19:18:18 +0000

    [add]ideal_new作成 ideals_controllerにストロングパラメータを作成

--- app/controllers/ideals_controller.rb
+++ app/controllers/ideals_controller.rb
@@ -6,6 +6,7 @@ class IdealsController < ApplicationController
 
   def create
     @ideal = Ideal.new(ideal_params)
+    @ideal.ideal_status = 0
     @ideal.seve
     redirect_to ideal_steps_path
   end
@@ -15,4 +16,11 @@ class IdealsController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def ideal_params
+    params.require(:ideal).permit(:text_1,:tect_2,:text_3)
+  end
+
 end
