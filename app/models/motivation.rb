ef3540b11cf75beb218a8a94ac6edadd804f061c
EC2 Default User <ec2-user@ip-172-31-13-194.ap-northeast-1.compute.internal>
2023-09-26 14:42:20 +0000

    [add]enumの日本語を入力

--- app/models/motivation.rb
+++ app/models/motivation.rb
@@ -2,6 +2,6 @@ class Motivation < ApplicationRecord
   belongs_to :user
   has_one :value,dependent: :destroy
 
-  enum motivation_status: { positive: 0, negative: 1, unpleasant_things: 2, to_do: 3, want: 4 }
+  enum motivation_status: { positive: 0, negative: 1, unpleasant: 2, to_do: 3, want: 4 }
 
 end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,7 +1,7 @@
 class Plan < ApplicationRecord
   belongs_to :schedule
 
-  enum priority_status: { hully: 0, important: 1, mission: 2 }
+  enum priority_status: { emergency: 0, important: 1, mission: 2 }
   enum progress_status: { start: 0, finish: 1, process: 2, postpone: 3, cancel: 4 }
 
 end
